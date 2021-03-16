6f2302f4e485bf4dc38ce47e2a6c845c9db2a91d
Hua Luo <luohuazju@gmail.com>
2021-03-15 23:07:06 -0500

    change the height

--- hammerjs/index.js
+++ hammerjs/index.js
@@ -3,13 +3,14 @@ var myElement = document.getElementById('myElement');
 // create a simple instance
 // by default, it only adds horizontal recognizers
 var mc = new Hammer(myElement);
+mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 // listen to events...
-mc.on("swipeleft swiperight", function(ev) {
+mc.on("panleft panright", function(ev) {
     myElement.textContent = ev.type +" gesture detected.";
 });
 
-mc.on("swipeup swipedown", function(ev) {
+mc.on("panup pandown", function(ev) {
     myElement.textContent = ev.type +" gesture detected.";
 });
 
